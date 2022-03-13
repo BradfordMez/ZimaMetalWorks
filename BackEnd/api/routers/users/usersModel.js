a9990f33a6b1fc0b0f6a8a705eb55531c2ca4310
Bradford-Meziere <coleokc@gmail.com>
2022-03-12 20:41:43 -0600

    Started scaffolding out the userRoute folder

---
+++ BackEnd/api/routers/users/usersModel.js
@@ -0,0 +1,34 @@
+const db = require('../../data/dbConfig');
+
+const getAllUsers = () => {
+    return db('users');
+};
+const getUserBy = (filter) => {
+    return db('users').where(filter);
+};
+const getUserById = (id) => {
+    return db('users').where({ id }).first();
+};
+const getUserByEmail = (email) => {
+    return db('users').where({ email }).first();
+};
+const createUser = async (user) => {
+    const [newUserObj] = await db('users').insert(user, [
+        'userID',
+        'name',
+        'password',
+        'email',
+        'number',
+        'admin',
+        'image',
+    ]);
+    return newUserObj;
+};
+
+module.exports = {
+    getAllUsers,
+    getUserBy,
+    getUserById,
+    getUserByEmail,
+    createUser,
+};
